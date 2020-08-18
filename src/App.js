b1902dbf512a973823b5dc33b9097d5be27ea379
ewecke <edgargandolfi@gmail.com>
2020-08-19 01:16:09 +0200

    not-working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 
-import React from 'react';
+import "./main.css"
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    lottoNunbers: [],
+    reset: null
+  };
+
+
+ newNumbers = () => {
+
+    const result = [];
+    let rNumber= 0;
+
+    for (let i = 0; i < 6; i++) {
+        rNumber = Math.floor(Math.random(1) * 49);
+        if (result.includes(rNumber)) {
+            rNumber = Math.floor(Math.random(1) * 49);
+            i--;
+        } else {
+            result.push(rNumber);    
+        }
+    }
+    console.log(result[0]);
+    console.log(result);
+/*     this.setState({ lottoNunbers: result }); */
+};
+
+  render() {
+    return (
+      <div className="App">
+        <div className="content">
+          <h1>Generating lucky number</h1>
+          <div className="container">
+            <div className="lotto"> {this.newNumbers()}</div>
+            <div className="lotto"> 02</div>
+            <div className="lotto"> 03</div>
+            <div className="lotto"> 04</div>
+            <div className="lotto"> 05</div>
+            <div className="lotto"> 06</div>
+            {/* <div className="glorynumber">01</div> */}
+          </div>
+          <button>Reset</button>
+          <button>Show me luck numbers</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-
-export default App;
+/* {console.log(item1[0])};
+{console.log(item1)};
+{console.log(newNumbers())}; */
